d5d31518ba687856308057adb0d1955110a65ec5
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-30 14:32:08 +0800

    login API

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userModel = mongoose.Schema(
     {
@@ -14,6 +15,21 @@ const userModel = mongoose.Schema(
         timestamps: true,
     }
 );
+
+// check if the entered password match with existing one
+userModel.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+// bcrypt before saving the password
+userModel.pre('save', async function (next) {
+    if(!this.isModified) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userModel);
 
