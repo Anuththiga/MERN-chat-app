9f40eb6461c15f0e169b85e13fb21f1d778b29d3
Tanuththiga <anuththiga25jt@gmail.com>
2023-10-01 13:12:02 +0800

    state initialization

--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -1,13 +1,26 @@
 import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [show, setShow] = useState(false);
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [confirmpassword, setConfirmpassword] = useState();
+    const [image, setImage] = useState();
+
+    const handleClick = () => setShow(!show);
+    const postDetails = (pics) => {};
+    const submit = () => {};
+
   return (
     <VStack spacing="5px">
         <FormControl id="firstname" isRequired>
             <FormLabel>Name</FormLabel>
             <Input
                 placeholder="Enter your name"
+                onChange={(e) => setName(e.target.value)}
             />
         </FormControl>
         <FormControl id="email" isRequired>
@@ -15,30 +28,35 @@ const SignUp = () => {
             <Input
                 type="email"
                 placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
             />
         </FormControl>
         <FormControl id="password" isRequired>
             <FormLabel>Password</FormLabel>
             <InputGroup size="md">
                 <Input 
+                    type={show ? "text" : "password"}
                     placeholder="Enter Password"
+                    onChange={(e) => setPassword(e.target.value)}
                 />   
                 <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm">
-                        Hide
+                    <Button h="1.75rem" size="sm" onClick={handleClick}>
+                        {show ? "Hide" : "Show"}
                     </Button>
                 </InputRightElement>      
             </InputGroup>
         </FormControl>
-        <FormControl id="password" isRequired>
+        <FormControl id="confirm-password" isRequired>
             <FormLabel>Confirm Password</FormLabel>
             <InputGroup size="md">
                 <Input
+                    type={show ? "text" : "password"}
                     placeholder="Confirm password"
+                    onChange={(e) => setConfirmpassword(e.target.value)}
                 />
                 <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm">
-                        Hide
+                    <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? "Hide" : "Show"}
                     </Button>
                 </InputRightElement>
             </InputGroup>
@@ -49,12 +67,14 @@ const SignUp = () => {
                 type="file"
                 p={1.5}
                 accept="image/*"
+                onChange={(e) => postDetails(e.target.files[0])}
             />
         </FormControl>
         <Button
             width="100%"
             colorScheme="blue"
             style={{ marginTop: 15 }}
+            onClick={submit}
         >
             Sign Up
         </Button>
