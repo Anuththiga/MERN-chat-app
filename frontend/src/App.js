5f418ef94c7c48d2bafa6d6edaab72961ec51d15
Tanuththiga <anuththiga25jt@gmail.com>
2023-10-01 11:32:31 +0800

    signup UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
