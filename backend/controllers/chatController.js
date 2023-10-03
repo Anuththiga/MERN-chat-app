c16763a32e70a0a2694b0304df31c56c03a03627
Tanuththiga <anuththiga25jt@gmail.com>
2023-10-03 12:52:50 +0800

    create group chat api

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -75,6 +75,44 @@ const fetchAllChats = asyncHandler(async (req, res) => {
         throw new Error(error.message);
     }
     
-})
+});
 
-module.exports = { createChat, fetchAllChats };
+const createGroupChat = asyncHandler(async (req, res) => {
+    if(!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please Fill all the fields" });
+    }
+
+    var users = JSON.parse(req.body.users);
+
+    if(users.length < 2) {
+        return res
+        .status(400)
+        .send("More than 2 users are required to form a group chat");
+    }
+
+    //push the loggedIn user as well
+    users.push(req.user);
+
+    try {
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            isGroup: true,
+            users: users,
+            groupAdmin: req.user,
+        });
+
+        //fetch the group chat from DB and send that back to api
+
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+                                    .populate("users", "-password")
+                                    .populate("groupAdmin", "-password");
+
+        res.status(200).json(fullGroupChat);
+
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+module.exports = { createChat, fetchAllChats, createGroupChat };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { createChat, fetchAllChats } = require("../controllers/chatController");
+const { createChat, fetchAllChats, createGroupChat } = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, createChat);
 router.route("/").get(protect, fetchAllChats);
+router.route("/group").post(protect, createGroupChat);
 
 module.exports = router;
