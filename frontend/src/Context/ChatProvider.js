1aaac7493eb18b3f2bd47db0e9c02f80726fce36
Anuththiga <anuththiga25jt@gmail.com>
2023-10-17 14:26:05 +0530

    chat notification

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -7,6 +7,7 @@ const ChatProvider = ({ children }) => {
     const [user, setUser] = useState();
     const [selectedChat, setSelectedChat] = useState();
     const [chats, setChats] = useState([]);
+    const [notification, setNotification] = useState([]);
 
     const navigate = useNavigate();
 
@@ -26,7 +27,9 @@ const ChatProvider = ({ children }) => {
                 selectedChat,
                 setSelectedChat,
                 chats, 
-                setChats
+                setChats,
+                notification, 
+                setNotification
             }}
         >
             {children}
